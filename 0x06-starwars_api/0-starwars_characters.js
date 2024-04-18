43adf582ffdeddac8816516d1016802a87f5621f
riley-kyule <phillipkyule@gmail.com>
2024-04-18 22:22:15 +0300

    Starwars v?

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,24 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as  list in the /films/ endpoint
- */
 
 const request = require('request');
+const MyArgs = process.argv.slice(2);
 
-const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${MyArgs}`;
 
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
+request(url, async (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+    return console.log(error);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode, response.statusMessage);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-
-  // Display characters in the same order as the "characters" list in the /films/ endpoint
-  filmData.characters.forEach((characterUrl) => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error('Error:', characterError);
-        process.exit(1);
-      }
-
-      if (characterResponse.statusCode !== 200) {
-        console.error('Error:', characterResponse.statusCode, characterResponse.statusMessage);
-        process.exit(1);
-      }
-
-      const characterData = JSON.parse(characterBody);
-      console.log(characterData.name);
+  const res = JSON.parse(body).characters;
+  for (let i = 0; i < res.length; i++) {
+    await new Promise((resolve, reject) => {
+      request(res[i], (e, resp, bod) => {
+        if (e) {
+          reject(e);
+        }
+        console.log(JSON.parse(bod).name);
+        resolve();
+      });
     });
-  });
+  }
 });
