d63d9ce94057cb3f0fbb5620d41786e14f2d595e
riley-kyule <phillipkyule@gmail.com>
2024-04-18 23:00:12 +0300

    Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,25 @@
 #!/usr/bin/node
+// A script that prints all characters of a Star Wars movie
+
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <n>');
-  process.exit(1);
-}
+const movie = process.argv[2];
+const api = 'https://swapi-api.hbtn.io/api/';
+const url = api + 'films/' + movie + '/';
+request.get({ url: url }, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    order(characters);
+  }
+});
 
-const id = process.argv[2];
-if (isNaN(id)) {
-  console.log('<n> must be an integer');
-  process.exit(1);
-}
-
-function promiseRequest (url) {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
+function order (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (err, res, body) {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        order(characters);
       }
-      resolve(JSON.parse(body));
     });
-  });
-}
-
-async function printChars () {
-  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-  try {
-    const response = await promiseRequest(url);
-    const characters = response.characters;
-    for (const character of characters) {
-      const body = await promiseRequest(character);
-      const name = body.name;
-      console.log(name);
-    }
-  } catch (err) {
-    console.error(err);
   }
 }
-
-printChars();
