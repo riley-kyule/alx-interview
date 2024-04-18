1a1ce8563380f6a7b912554fe78ced873db88170
riley-kyule <phillipkyule@gmail.com>
2024-04-18 22:20:34 +0300

    Starwars v?

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,50 @@
 #!/usr/bin/node
+/**
+ * Prints all characters of a Star Wars movie
+ * The first positional argument passed is the Movie ID
+ * Display one character name per line in the same order
+ * as  list in the /films/ endpoint
+ */
 
 const request = require('request');
+
 const movieId = process.argv[2];
 
-function getMovieCharacters(movieId) {
-  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
-
-  request.get(filmsUrl, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
-
-      if (movieData) {
-        const charactersUrls = movieData.characters;
-        const characters = [];
-
-        for (const charUrl of charactersUrls) {
-          request.get(charUrl, (error, response, body) => {
-            if (response.statusCode === 200) {
-              const characterData = JSON.parse(body);
-              characters.push(characterData.name);
-              if (characters.length === charactersUrls.length) {
-                printCharacters(characters, movieData.title);
-              }
-            } else {
-              console.log(`Error: ${response.statusCode}`);
-            }
-          });
-        }
-      } else {
-        console.log(`Movie ID ${movieId} not found.`);
-      }
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
-  });
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
-function printCharacters(characters, movieTitle) {
-  console.log(`Characters in ${movieTitle}:`);
-  for (const character of characters) {
-    console.log(character);
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
   }
-}
 
-if (movieId) {
-  getMovieCharacters(movieId);
-} else {
-  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
-}
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode, response.statusMessage);
+    process.exit(1);
+  }
+
+  const filmData = JSON.parse(body);
+
+  // Display characters in the same order as the "characters" list in the /films/ endpoint
+  filmData.characters.forEach((characterUrl) => {
+    request(characterUrl, (characterError, characterResponse, characterBody) => {
+      if (characterError) {
+        console.error('Error:', characterError);
+        process.exit(1);
+      }
+
+      if (characterResponse.statusCode !== 200) {
+        console.error('Error:', characterResponse.statusCode, characterResponse.statusMessage);
+        process.exit(1);
+      }
+
+      const characterData = JSON.parse(characterBody);
+      console.log(characterData.name);
+    });
+  });
+});
